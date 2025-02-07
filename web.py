0017e53c1a27512e1a68b7837a67cfbb368eb76f
chaitanyanextq <chaitanya.tajne@nextq.ai>
2025-02-07 16:18:33 +0530

    Update web.py

--- web.py
+++ web.py
@@ -26,7 +26,7 @@ with title_right:
     try:
         logo_path = "Screenshot 2025-02-04 193738.png"  # Ensure this is the correct relative path
         logo = Image.open(logo_path)
-        st.image(logo, width=200)
+        st.image(logo, width=300)
     except Exception:
         st.warning("Logo not found!")
 
